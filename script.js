4f0b58ecf889f15e25aa38bbce77d8151c701352
shawn <173310929+shawntarou@users.noreply.github.com>
2025-04-13 12:57:15 -0700

    Add autoScroll behavior

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function deleteLastLine() {
         last_line.remove();
     }
 }
+
+function autoScroll() {
+    const LEEWAY = 500;
+    let body = document.body;
+    let offset = body.scrollHeight - body.offsetHeight;
+    let scrollPos = (body.scrollTop + offset);
+    let scrollBottom = (body.scrollHeight - (body.clientHeight + offset));
+    if (scrollPos >= scrollBottom - LEEWAY) {
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+}
+
+const observer = new MutationObserver(autoScroll);
+
+observer.observe(document.body, { childList: true, attributes: false });
